3ee41fd0f9cf4eadca67913a7717e93f244a7523
Nick Mixdorf <namixdorf@gmail.com>
2012-01-19 18:12:37 -0600

    removed index and generated store scaffold

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
+  validates_length_of :title, :minimum => 10,
+      message: "must be at least 10 characters long"
+
   validates :image_url, allow_blank: true, format: {
       with: %r{\.(gif|jpg|png)$}i,
       message: 'must be a URL for GIF, JPG or PNG image.'
